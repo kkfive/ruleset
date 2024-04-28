b12c9b62a9c2344b26a1de464aa9530938148bc9
DreamyTZK <i@tzki.cn>
2024-04-28 22:48:24 +0800

    update

--- dnsmap.ts
+++ dnsmap.ts
@@ -3,21 +3,35 @@ import { writeFileSync } from './utils'
 import db from '@/db/generate.dns'
 
 const fileList = db
-const regex = /\[Host\]([^\[]+)/g
 async function main() {
   fileList.forEach((fileItem) => {
     const listMap: Map<string, string> = new Map()
     fileItem.includeFiles.forEach((file) => {
-      const data = fs.readFileSync(file, 'utf-8')
-      const data2 = data.match(regex)?.length ? data.match(regex)![0] : ''
-      const lines = data2.replace('[Host]', '').split('\n').filter(i => !!i).filter(i => !(i.startsWith('#') || i.startsWith('//')))
-      lines.forEach((line) => {
-        line = line.trim()
-        const line2 = line.split('=')
-        if (line2.length === 2) {
-          const isHasMark = line2[1].split('// ')
-          const dns = isHasMark.length === 2 ? isHasMark[0].trim() : line2[1].trim()
-          listMap.set(line2[0].trim(), dns)
+      const content = fs.readFileSync(file, 'utf-8')
+      const body = content.match(/[^\r\n]+/g)
+      body?.forEach((x) => {
+        x = x
+          .trim()
+          .replace(/^(#|;|\/\/)\s*/, '#')
+          .replace(/\s+[^\s]+\s+url-and-header\s+/, ' url ')
+          .replace(/(^[^#].+)\x20+\/\/.+/, '$1')
+          .replace(/^#!PROFILE-VERSION-REQUIRED\s+[0-9]+\s+/i, '')
+          .replace(/^(#)?host-wildcard\s*,.+/i, '')
+          .replace(/^(#)?host(-suffix|-keyword|)?\s*,\s*/i, '$1DOMAIN$2,')
+          .replace(/^(#)?ip6-cidr\s*,\s*/i, '$1IP-CIDR6,')
+          .replace('server:system', 'server:syslib')
+
+        const result = /^#?(?:\*|localhost|[-*?0-9a-z]+\.[-*.?0-9a-z]+)\s*=\s*(?:sever\s*:\s*|script\s*:\s*)?[\s0-9a-z:/,.]+$/g.test(x)
+        const result2 = /^(?:#\s*)?(?:\*|localhost|(?:(?:[\w?*-]+\.)+(?:[\w-]+))|\b(?:(?:\d{1,3}\.){3}\d{1,3}(?:\.in-addr\.arpa)?)\b)\s*=\s*(?:server\s*:|script\s*:)?\s*(?:https?|quic|system):\/\/(?:(?:[\w-]+\.)+[\w-]+|(?:\d{1,3}\.){3}\d{1,3})(?::\d+)?\/?(?:[^\s#]*)(?:\?[^\s#]*)?$/g.test(x)
+
+        if (!x.startsWith('#')) {
+          if (result || result2) {
+            const [key, value] = x.split('=')
+            if (listMap.has(key.trim()) && listMap.get(key.trim()) !== value.trim())
+              console.warn(`${key.trim()}重复了,本次值为${value.trim()}，上次值为${listMap.get(key.trim())}`)
+            else
+              listMap.set(key.trim(), value.trim())
+          }
         }
       })
     })
