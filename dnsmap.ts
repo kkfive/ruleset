afcdcd720df8b82aa3b60f8a6f3ed6bb261d3e06
DreamyTZK <i@tzki.cn>
2024-04-29 15:05:03 +0800

    update

--- dnsmap.ts
+++ dnsmap.ts
@@ -3,10 +3,107 @@ import YAML from 'yaml'
 import { writeFileSync } from './utils'
 import db from '@/db/generate.dns'
 
+const defaultList = {
+  'dns.google': '8.8.8.8, 8.8.4.4, 2001:4860:4860::8888, 2001:4860:4860::8844',
+  'dns64.dns.google': '2001:4860:4860::6464, 2001:4860:4860::64',
+  'cloudflare-dns.com': '104.16.249.249, 104.16.248.249, 2606:4700::6810:f8f9, 2606:4700::6810:f9f9',
+  '1dot1dot1dot1.cloudflare-dns.com': '1.1.1.1, 1.0.0.1, 2606:4700:4700::1001, 2606:4700:4700::1111',
+  'one.one.one.one': '1.1.1.1, 1.0.0.1, 2606:4700:4700::1001, 2606:4700:4700::1111',
+  'dns.alidns.com': '223.5.5.5, 223.6.6.6, 2400:3200:baba::1, 2400:3200::1',
+  'doh.pub': '1.12.12.12, 120.53.53.53',
+  'dot.pub': '1.12.12.12, 120.53.53.53',
+  'doh.360.cn': '23.6.48.18, 112.65.69.15',
+  'dot.360.cn': '101.198.198.198, 101.198.199.200, 101.198.192.33, 112.65.69.15',
+  'dns.twnic.tw': '101.101.101.101, 2001:de4::101, 2001:de4::102',
+  'ordns.he.net': '74.82.42.42, 2001:470:20::2',
+  'talk.google.com': '108.177.125.188',
+  'mtalk.google.com': '108.177.125.188, 2404:6800:4008:c07::bc, 142.250.31.188',
+  'alt1-mtalk.google.com': '3.3.3.3, 2607:f8b0:4023:c0b::bc, 64.233.171.188',
+  'alt2-mtalk.google.com': '3.3.3.3, 142.250.115.188',
+  'alt3-mtalk.google.com': '74.125.200.188, 173.194.77.188',
+  'alt4-mtalk.google.com': '74.125.200.188, 173.194.219.188',
+  'alt5-mtalk.google.com': '3.3.3.3, 2607:f8b0:4023:1::bc, 142.250.112.188',
+  'alt6-mtalk.google.com': '3.3.3.3, 172.217.197.188',
+  'alt7-mtalk.google.com': '74.125.200.188, 2607:f8b0:4002:c03::bc, 108.177.12.188',
+  'alt8-mtalk.google.com': '3.3.3.3',
+
+  // 阿里云
+  '*.alicdn.com': 'server:quic://dns.alidns.com',
+  '*.aliyuncs.com': 'server:quic://dns.alidns.com',
+  '*.alikunlun.com': 'server:quic://dns.alidns.com',
+  '*.alipay.com': 'server:quic://dns.alidns.com',
+  '*.alipay.com.cn': 'server:quic://dns.alidns.com',
+  '*.alipayobjects.com': 'server:quic://dns.alidns.com',
+  '*.alibabausercontent.com': 'server:quic://dns.alidns.com',
+  '*.aliyundrive.com': 'server:quic://dns.alidns.com',
+  '*.1688.com': 'server:quic://dns.alidns.com',
+  'tb.cn': 'server:quic://dns.alidns.com',
+  '*.taobao.com': 'server:quic://dns.alidns.com',
+  '*.tbcache.com': 'server:quic://dns.alidns.com',
+  '*.tmall.com': 'server:quic://dns.alidns.com',
+  '*.mmstat.com': 'server:quic://dns.alidns.com',
+  '*.aliapp.org': 'server:quic://dns.alidns.com',
+  'upos-sz-mirrorali.bilivideo.com': 'server:quic://dns.alidns.com',
+
+  // 腾讯
+
+  '*.qcloud.com': 'server:https://doh.pub/dns-query',
+  '*.gtimg.cn': 'server:https://doh.pub/dns-query',
+  '*.idqqimg.com': 'server:https://doh.pub/dns-query',
+  '*.myqcloud.com': 'server:https://doh.pub/dns-query',
+  '*.qpic.cn': 'server:https://doh.pub/dns-query',
+  '*.qlogo.cn': 'server:https://doh.pub/dns-query',
+  '*.qq.com': 'server:https://doh.pub/dns-query',
+  '*.tencent-cloud.net': 'server:https://doh.pub/dns-query',
+  '*.tencent.com': 'server:https://doh.pub/dns-query',
+  '*.weixinbridge.com ': 'server:https://doh.pub/dns-query',
+  '*.weixin.com': 'server:https://doh.pub/dns-query',
+  '*.wechat.com': 'server:https://doh.pub/dns-query',
+  '*.weiyun.com': 'server:https://doh.pub/dns-query',
+  '*.cdn-go.cn': 'server:https://doh.pub/dns-query',
+  '*.qcloudimg.com': 'server:https://doh.pub/dns-query',
+  'url.cn': 'server:https://doh.pub/dns-query',
+  '*.weixinbridge.com': 'server:https://doh.pub/dns-query',
+
+  // 字节 180.184.1.1 或 180.184.2.2
+  '*.toutiaocloud.com': 'server:180.184.1.1',
+  '*.toutiaopage.com': 'server:180.184.1.1',
+  '*.feiliao.com': 'server:180.184.1.1',
+  '*.iesdouyin.com': 'server:180.184.1.1',
+  '*.pstatp.com': 'server:180.184.1.1',
+  '*.snssdk.com': 'server:180.184.1.1',
+  '*.bytegoofy.com': 'server:180.184.1.1',
+  '*.toutiao.com': 'server:180.184.1.1',
+  '*.douyin.com': 'server:180.184.1.1',
+  '*.douyinpic.com': 'server:180.184.1.1',
+  '*.douyinstatic.com': 'server:180.184.1.1',
+  '*.douyinvod.com': 'server:180.184.1.1',
+  '*.huoshan.com': 'server:180.184.1.1',
+  '*.huoshanzhibo.com': 'server:180.184.1.1',
+  '*.ixigua.com': 'server:180.184.1.1',
+  '*.byted-static.com': 'server:180.184.1.1',
+  // 360
+  '*.qhimg.com': 'server:https://doh.360.cn/dns-query',
+  '*.qhres.com': 'server:https://doh.360.cn/dns-query',
+  '*.qhmsg.com': 'server:https://doh.360.cn/dns-query',
+  '*.qhupdate.com': 'server:https://doh.360.cn/dns-query',
+  '*.360.cn': 'server:https://doh.360.cn/dns-query',
+  '*.360safe.com': 'server:https://doh.360.cn/dns-query',
+  '*.360webcache.com': 'server:https://doh.360.cn/dns-query',
+  '*.360kuai.com': 'server:https://doh.360.cn/dns-query',
+  '*.so.com': 'server:https://doh.360.cn/dns-query',
+  '*.yunpan.cn': 'server:https://doh.360.cn/dns-query',
+  '*.yunpan.com': 'server:https://doh.360.cn/dns-query',
+  '*.yunpan.com.cn': 'server:https://doh.360.cn/dns-query',
+}
+
 const fileList = db
 async function main() {
   fileList.forEach((fileItem) => {
     const listMap: Map<string, string> = new Map()
+    Object.entries(defaultList).forEach(([key, value]) => {
+      listMap.set(key, value)
+    })
     fileItem.includeFiles.forEach((file) => {
       const content = fs.readFileSync(file, 'utf-8')
       const body = content.match(/[^\r\n]+/g)
@@ -28,10 +125,22 @@ async function main() {
         if (!x.startsWith('#')) {
           if (result || result2) {
             const [key, value] = x.split('=')
-            if (listMap.has(key.trim()) && listMap.get(key.trim()) !== value.trim())
-              console.warn(`${key.trim()}重复了,本次值为${value.trim()}，上次值为${listMap.get(key.trim())}`)
-            else
-              listMap.set(key.trim(), value.trim())
+            if (listMap.has(key.trim()) && listMap.get(key.trim()) !== value.trim()) {
+              // eslint-disable-next-line ts/ban-ts-comment
+              // @ts-expect-error
+              if (!defaultList[key.trim()])
+                console.warn(`${key.trim()}重复了,本次值为${value.trim()}，上次值为${listMap.get(key.trim())}`)
+            }
+            else {
+              listMap.set(
+                key.trim(),
+                value.trim()
+                  .replace('server:119.29.29.29', 'server:https://doh.pub/dns-query')
+                  .replace('server:quic://223.6.6.6:853', 'server:quic://dns.alidns.com')
+                  .replace('server:quic://223.5.5.5:853', 'server:quic://dns.alidns.com')
+                  .replace('server:https://120.53.53.53/dns-query', 'server:https://doh.pub/dns-query'),
+              )
+            }
           }
         }
       })
@@ -45,11 +154,20 @@ main()
 
 function writeClash(listMap: Map<string, string>) {
   const payload: Record<string, string[]> = {}
+  const hostPayload: Record<string, string[]> = {
+    'talk.google.com': ['108.177.125.188'],
+    'alt8-mtalk.google.com': ['3.3.3.3'],
+  }
   listMap.forEach((value, key) => {
-    payload[key] = [value.split(',')[0].replace('server:', '').replace('syslib', 'system')]
+    const newValue = value.split(',').map(item => item.trim().replace('server:', '').replace('syslib', 'system'))
+    if (newValue.length > 1)
+      hostPayload[key] = newValue
+    else if (!hostPayload[key])
+      payload[key] = value.split(',').map(item => item.trim().replace('server:', '').replace('syslib', 'system'))
   })
 
   writeFileSync(`./dist/clash/surgio/dnsMap.snippet`, YAML.stringify(payload))
+  writeFileSync(`./dist/clash/surgio/dnsMap.hosts.snippet`, YAML.stringify(hostPayload))
 }
 function writeSurge(listMap: Map<string, string>) {
   const list: string[] = []
@@ -61,6 +179,7 @@ function writeSurge(listMap: Map<string, string>) {
 #!desc=1.域名按所属DNS进行分流解析 2.路由器地址使用系统DNS进行解析 3.特殊地址使用规范DNS进行解析
 #!author=DreamyTZK
 #!category=DreamyTZK
+#!update:${new Date().toISOString()}
 
 [General]
 # ---(DNS 服务器)---
