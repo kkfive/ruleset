0d76d1e76ec311dea1e8c65c341859c1c31d2ff3
DreamyTZK <i@tzki.cn>
2024-05-02 18:02:45 +0800

    update

---
+++ src/build/disableHostName.ts
@@ -0,0 +1,25 @@
+/**
+ * 一些域名不可被 MitM 代理，这里可以添加一些域名
+ */
+
+import { CONF } from '@/config'
+import { writeFileSync } from '@/utils'
+
+const list = [
+  // apple
+  'gateway.icloud.com',
+  'gateway.icloud.com.cn',
+  'weather-data.apple.com',
+  'buy.itunes.apple.com',
+  // mac 端 百度云盘客户端
+  'pan.baidu.com',
+  'update.pan.baidu.com',
+  // tg 客户端
+  '95.161.76.101',
+  // 京东
+  'www.jd.com',
+];
+
+(() => {
+  writeFileSync(`${CONF.outputDir}/surge/surgio/disable-host-name`, list.join(','))
+})()
